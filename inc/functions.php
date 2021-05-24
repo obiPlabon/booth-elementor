91c6b655841a315afd9ad8f100bb688f188fbd3d
obiPlabon <obiplabon@gmail.com>
2021-05-24 23:08:05 +0600

    Cleanup

--- inc/functions.php
+++ inc/functions.php
@@ -4,21 +4,6 @@
  */
 defined( 'ABSPATH' ) || die();
 
-// echo \Elementor\Plugin::$instance->frontend->get_builder_content_for_display( '2620' );
-
-if (!function_exists('litipay_get_wysiwyg_output')) {
-	function litipay_get_wysiwyg_output($meta_key) {
-		global $wp_embed;
-
-		$content = $wp_embed->autoembed($meta_key);
-		$content = $wp_embed->run_shortcode($content);
-		$content = do_shortcode($content);
-		$content = wpautop($content);
-
-		return $content;
-	}
-}
-
 /**
  * Get elementor instance.
  *
